9dca95da3645aa86c03e7033da280c6372c5f5b4
mikkelriis8 <114573302+mikkelriis8@users.noreply.github.com>
2022-11-16 23:30:13 +0100

    Add files via upload

--- tests/src/tests.cpp
+++ tests/src/tests.cpp
@@ -11,18 +11,23 @@ extern "C"
 
 // See Catch2's documentation: https://github.com/catchorg/Catch2/blob/devel/docs/tutorial.md#scaling-up
 
+/* Exercise 3 */
 TEST_CASE("sumtail")
 {
-    // Add the tests for excercise 3 "sumtail" function here. Use "REQUIRE()" statement to check. 
-    REQUIRE(1==0);
+    REQUIRE(sumtail(1,0)==1); // sumwhile(1) must be 1
+    REQUIRE(sumtail(7,0)==28); // sumwhile(33) =1+2+3+4+5+6+7 = 28
+    REQUIRE(sumtail(33,0)==561); // sumwhile(33) =1+2+3+...+33 = 561
 }
 
+/* Exercise 3 */
 TEST_CASE("sumwhile")
 {
-    // Add the tests for excercise 3 "sumwhile" function here. Use "REQUIRE()" statement to check. 
-    REQUIRE(1==0);
+    REQUIRE(sumtail(1,0)==1); // sumwhile(1) must be 1
+    REQUIRE(sumtail(7,0)==28); // sumwhile(7) = 1+2+3+4+5+6+7 = 28
+    REQUIRE(sumtail(33,0)==561); // sumwhile(33) =1+2+3+...+33 = 561
 }
 
+/* Exercise 2 */
 TEST_CASE("sumn")
 {
     REQUIRE(sumn(1)==1); // sum(1) must be 1
@@ -30,10 +35,13 @@ TEST_CASE("sumn")
     REQUIRE(sumn(33)==1089); //sum(33) = 1+3+5+...+65 = 1089
 }
 
+/* Exercise 4 */
 TEST_CASE("fib")
 {
-   // Add the tests for excercise 4 "fib" function here. Use "REQUIRE()" statement to check. 
-    REQUIRE(1==0);
+    REQUIRE(fib(1,0,1)==1); // fib(1) must be 1 (base case)
+    REQUIRE(fib(2,0,1)==1); // fib(2) must be 1 as well (base case)
+    REQUIRE(fib(7,0,1)==13); 
+    REQUIRE(fib(15,0,1)==610);
 }
 
 
